5b67820c942cb5107bba53e54d5cad19f4d56978
rosemell <109337532+rosemell@users.noreply.github.com>
2023-06-05 22:51:00 +0800

    Add files via upload

---
+++ 最优解显得我很呆/环状链表2.c
@@ -0,0 +1,78 @@
+https://leetcode.cn/problems/linked-list-cycle-ii/description/
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     struct ListNode *next;
+ * };
+ */
+
+//方法3：将相遇点记录并置空，从相遇点下个点开始为头看作新链表，入口就是新链表与原链表的第一个重合点
+
+//抄的
+struct ListNode *detectCycle(struct ListNode *head){
+     if(NULL==head||NULL==head->next)
+   {
+        return NULL;
+   }
+    struct ListNode*fast=head,*slow=head;
+    while(fast&&fast->next)//让fast和low进入环并处于同一位置
+    {
+        
+        fast=fast->next->next;
+        slow=slow->next;
+        if(slow==fast)
+        {
+            fast=head;
+            while(fast!=slow)
+    {
+        fast=fast->next;
+        slow=slow->next;
+    }return fast;
+        }
+
+    }
+    
+    return NULL;
+}
+
+
+//自己一开始学了环形链表1做出来的低速方法
+// struct ListNode *detectCycle(struct ListNode *head) {
+//     if(NULL==head||NULL==head->next)
+//    {
+//         return NULL;
+//    }
+//     struct ListNode*fast=head->next->next,*slow=head;
+//     while(fast!=slow)//让fast和low进入环并处于同一位置
+//     {
+//         if(NULL==fast||NULL==fast->next)
+//         {
+//             return NULL;
+//         }
+//         fast=fast->next->next;
+//         slow=slow->next;
+//     }
+
+//     int i=1;//让fast追一圈slow，数一下圈中元素个数
+//     fast=fast->next->next;
+//     slow=slow->next;
+//     for(i=1;fast!=slow;i++)
+//     {
+//         fast=fast->next->next;
+//         slow=slow->next;
+//     }
+//     fast=head;
+//     slow=head;
+//     for(;i;i--)
+//     {
+//         fast=fast->next;
+//     }
+//     while(fast!=slow)
+//     {
+//         fast=fast->next;
+//         slow=slow->next;
+//     }
+//      return fast;
+// }
